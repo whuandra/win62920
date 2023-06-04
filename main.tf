2836f937e0f5f075bf49a8efa3d2f11069ac16d4
whuandra <50607920+whuandra@users.noreply.github.com>
2023-06-04 16:31:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "winbucket" {
-    bucket = "winoto62920"
-    acl = "public-read"
-  
-}
-
 resource "aws_s3_bucket" "winfolder" {
     bucket = "winoto62920evening"
     acl = "public-read"
