2b90749cc5d8266f0503d4c14651fe0871df27bd
whuandra <50607920+whuandra@users.noreply.github.com>
2023-06-04 19:45:40 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+resource "aws_instance" "Windows" {
+    ami = "ami-0ab05a04b66a879af"
+    instance_type = "m5.2xlarge"
+    
+    tags = {
+        Name = "Test Server"
+    }
+}
+   
+    
