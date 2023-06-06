30f7c49780bf186197a456746ec5c28ba8da44c2
whuandra <50607920+whuandra@users.noreply.github.com>
2023-06-05 20:34:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,17 @@ provider "aws" {
     region = "us-east-1"
 }
 
+variable "aws_resources" {
+    type = "map"
+    default = {
+        keyname = "key41220good"
+    }
+}
+
 resource "aws_instance" "Windows" {
     ami = "ami-0ab05a04b66a879af"
     instance_type = "m5.2xlarge"
+    key_name = lookup(var.aws_resources, "keyname")
     
     tags = {
         Name = "Test Server"
